6e25535e05a6777d5956416a8a8c548d9025436f
Mahmut Coskun <mahmutcoskunmailbox@gmail.com>
2024-01-25 00:27:30 +0300

    project done in one commit :)

---
+++ Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> listClients = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=mystore;Integrated Security=True";
+            
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection)) 
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader()) 
+                        {
+                            while (reader.Read()) 
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+                            
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string created_at { get; set;}
+
+    }
+}
